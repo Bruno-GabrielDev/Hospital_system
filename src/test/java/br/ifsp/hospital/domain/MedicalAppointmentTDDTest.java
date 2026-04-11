87db72759eb9b87901da3e57b3a0cbb0b77ada7e
Eduardo Ferraz <dudiferraz@gmail.com>
2026-04-11 17:20:29 -0300

    test(appointment): add failing test for #43 cancel scheduled appointment

--- src/test/java/br/ifsp/hospital/domain/MedicalAppointmentTDDTest.java
+++ src/test/java/br/ifsp/hospital/domain/MedicalAppointmentTDDTest.java
@@ -1,4 +1,21 @@
 package br.ifsp.hospital.domain;
 
+import br.ifsp.hospital.annotation.TDD;
+import br.ifsp.hospital.annotation.UnitTest;
+import br.ifsp.hospital.domain.model.AppointmentStatus;
+import br.ifsp.hospital.domain.model.MedicalAppointment;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class MedicalAppointmentTDDTest {
+
+    @Test
+    @UnitTest
+    @TDD
+    void shouldCancelOpenAppointment(){
+        MedicalAppointment appointment = MedicalAppointment.of(null, null, null);
+
+        appointment.cancel();
+        assertThat(appointment.getStatus()).isEqualTo(AppointmentStatus.CANCELED);
+    }
 }
