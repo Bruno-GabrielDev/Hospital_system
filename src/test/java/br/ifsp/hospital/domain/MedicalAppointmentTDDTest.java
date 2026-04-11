fc329f3e3ddc532619f291ad850142e309787345
Eduardo Ferraz <dudiferraz@gmail.com>
2026-04-11 20:02:23 -0300

    refactor(appointment): use dummy objects instead of nulls in cancel test

--- src/test/java/br/ifsp/hospital/domain/MedicalAppointmentTDDTest.java
+++ src/test/java/br/ifsp/hospital/domain/MedicalAppointmentTDDTest.java
@@ -2,9 +2,11 @@ package br.ifsp.hospital.domain;
 
 import br.ifsp.hospital.annotation.TDD;
 import br.ifsp.hospital.annotation.UnitTest;
-import br.ifsp.hospital.domain.model.AppointmentStatus;
-import br.ifsp.hospital.domain.model.MedicalAppointment;
+import br.ifsp.hospital.domain.model.*;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MedicalAppointmentTDDTest {
@@ -13,7 +15,11 @@ public class MedicalAppointmentTDDTest {
     @UnitTest
     @TDD
     void shouldCancelOpenAppointment(){
-        MedicalAppointment appointment = MedicalAppointment.of(null, null, null);
+        Patient dummyPatient = Patient.of("John Doe", "123456789", InsuranceType.BASIC);
+        Doctor dummyDoctor = Doctor.of("John Doe", "Cardiology", "54321");
+        LocalDateTime dummyScheduledAt = LocalDateTime.now().plusDays(1);
+
+        MedicalAppointment appointment = MedicalAppointment.of(dummyPatient, dummyDoctor, dummyScheduledAt);
 
         appointment.cancel();
         assertThat(appointment.getStatus()).isEqualTo(AppointmentStatus.CANCELED);
