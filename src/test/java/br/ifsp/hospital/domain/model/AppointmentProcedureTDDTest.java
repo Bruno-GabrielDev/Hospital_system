53d585aa459447d6279db8b49d6b4959b7df12e3
rhuanaa <rhuanandrey@gmail.com>
2026-04-12 16:52:59 -0300

    test(AppointmentProcedure): appointment procedure restore test, baby step for #6

---
+++ src/test/java/br/ifsp/hospital/domain/model/AppointmentProcedureTDDTest.java
@@ -0,0 +1,31 @@
+package br.ifsp.hospital.domain.model;
+
+import br.ifsp.hospital.annotation.TDD;
+import br.ifsp.hospital.annotation.UnitTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@UnitTest
+@TDD
+@DisplayName("TDD – AppointmentProcedure")
+class AppointmentProcedureTDDTest {
+
+    @Test
+    @DisplayName("#6 – restore deve retornar instância com valores corretos")
+    void shouldReturnAppointmentProcedureInstanceWithAllValuesCorrect() {
+        UUID id = UUID.randomUUID();
+        Procedure procedure = Procedure.of("RX", new Money(new BigDecimal("100.00")));
+        AppointmentProcedure restored = AppointmentProcedure.restore(id, procedure, 3);
+
+        assertThat(restored).isNotNull();
+        assertThat(restored.getId()).isEqualTo(id);
+        assertThat(restored.getQuantity()).isEqualTo(3);
+        assertThat(restored.getProcedure()).isEqualTo(procedure);
+    }
+
+}
