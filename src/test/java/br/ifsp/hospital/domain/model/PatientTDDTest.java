b4028c10664e6917b443382f8907f88718fdb642
rhuanaa <rhuanandrey@gmail.com>
2026-04-12 16:04:59 -0300

    test(patient): restore method test, baby step for #6 user story

---
+++ src/test/java/br/ifsp/hospital/domain/model/PatientTDDTest.java
@@ -0,0 +1,31 @@
+package br.ifsp.hospital.domain;
+
+import br.ifsp.hospital.annotation.TDD;
+import br.ifsp.hospital.annotation.UnitTest;
+import br.ifsp.hospital.domain.model.InsuranceType;
+import br.ifsp.hospital.domain.model.Patient;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@UnitTest
+@TDD
+@DisplayName("TDD – Patient")
+class PatientTDDTest {
+
+    @Test
+    @DisplayName("#6 – restore deve retornar instância com todos os valores corretos")
+    void shouldReturnPatinentInstanceWithAllValuesCorrect() {
+        UUID id = UUID.randomUUID();
+        Patient restored = Patient.restore(id, "Ana", "999.999.999-99", InsuranceType.PREMIUM);
+
+        assertThat(restored).isNotNull();
+        assertThat(restored.getId()).isEqualTo(id);
+        assertThat(restored.getName()).isEqualTo("Ana");
+        assertThat(restored.getDocument()).isEqualTo("999.999.999-99");
+        assertThat(restored.getInsuranceType()).isEqualTo(InsuranceType.PREMIUM);
+    }
+}
