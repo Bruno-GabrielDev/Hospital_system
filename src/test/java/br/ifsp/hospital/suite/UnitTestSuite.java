43c1002a07b1b2d2135564a86745dd670de7ca14
Bruno-GabrielDev <brunowz2001@gmail.com>
2026-04-14 16:07:14 -0300

    Add Unit test suite for unit tests - Created UnitTestSuite class with @Suite annotation - Configured to run all tests with @UnitTest tag from br.ifsp.hospital package - Provides organized execution of unit test cases

--- src/test/java/br/ifsp/hospital/suite/UnitTestSuite.java
+++ src/test/java/br/ifsp/hospital/suite/UnitTestSuite.java
@@ -1,4 +1,12 @@
 package br.ifsp.hospital.suite;
 
+import org.junit.platform.suite.api.*;
+
+
+@Suite
+@SuiteDisplayName("Suíte Unitária – Todos os testes @UnitTest")
+@SelectPackages("br.ifsp.hospital")
+@IncludeTags("UnitTest")
 public class UnitTestSuite {
 }
+
