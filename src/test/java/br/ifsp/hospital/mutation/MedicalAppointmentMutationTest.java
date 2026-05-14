5461b8dc8f48a3eca25a0e9db37d7258a9496c2e
Eduardo Ferraz <dudiferraz@gmail.com>
2026-05-14 20:21:25 -0300

    test(MedicalAppointment): matar mutante no método isReceiptIssued

---
+++ src/test/java/br/ifsp/hospital/mutation/MedicalAppointmentMutationTest.java
@@ -0,0 +1,32 @@
+package br.ifsp.hospital.mutation;
+
+import br.ifsp.hospital.annotation.Mutation;
+import br.ifsp.hospital.annotation.UnitTest;
+import br.ifsp.hospital.domain.model.Doctor;
+import br.ifsp.hospital.domain.model.InsuranceType;
+import br.ifsp.hospital.domain.model.MedicalAppointment;
+import br.ifsp.hospital.domain.model.Patient;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@UnitTest
+@Mutation
+@DisplayName("Mutation – MedicalAppointment")
+public class MedicalAppointmentMutationTest {
+
+    @Test
+    @DisplayName("Deve retornar false para isReceiptIssued em um agendamento recém-criado")
+    void isReceiptIssuedNewAppointmentShouldReturnFalse() {
+        Patient patient = Patient.of("John", "123", InsuranceType.BASIC);
+        Doctor doctor = Doctor.of("Dr. House", "Cardio", "CRM123");
+        LocalDateTime scheduledAt = LocalDateTime.now().plusDays(1);
+
+        MedicalAppointment appointment = MedicalAppointment.of(patient, doctor, scheduledAt);
+
+        assertThat(appointment.isReceiptIssued()).isFalse();
+    }
+}
